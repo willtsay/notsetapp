1f7d37fade9535aaef1f6b6e84edd0f1792b93fb
William Tsay <willtsay@gmail.com>
2014-09-10 13:07:06 -0700

    revamped QOZM buttons

--- app/assets/javascripts/ng-app/services/player.js
+++ app/assets/javascripts/ng-app/services/player.js
@@ -7,25 +7,25 @@ function Player($rootScope, $timeout, Game){
     {
       id: 0,
       color: 'blue',
-      text:"press 'Q' to attempt a set",
+      text:"Q",
       points: 0
     },
     {
       id: 1,
       color: 'red',
-      text:"press 'O' to attempt a set",
+      text:"O",
       points: 0
     },
     {
       id: 2,
       color: 'green',
-      text:"press 'Z' to attempt a set",
+      text:"Z",
       points: 0
     },
     {
       id: 3,
-      color: 'yellow',
-      text:"press 'M' to attempt a set",
+      color: 'purple',
+      text:"M",
       points:0
     }],
     cullPlayers: function(amount){
